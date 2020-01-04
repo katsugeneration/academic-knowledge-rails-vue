770abce5c312741f0c5df5d387f76232b5c87ea3
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2020-01-04 12:23:36 +0900

    Lint route hash

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :academic_search, only: [] do
     collection do
-      get '/', :action => :index
+      get "/", action: :index
     end
   end
 end
